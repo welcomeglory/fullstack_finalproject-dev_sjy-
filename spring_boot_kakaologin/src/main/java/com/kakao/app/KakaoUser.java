fb51d9a68d6096449836dd8bd88bfe17a4354d9c
welcomeglory <godbu2021@naver.com>
2024-05-29 21:29:29 +0900

    유저 클래스 생성

---
+++ spring_boot_kakaologin/src/main/java/com/kakao/app/KakaoUser.java
@@ -0,0 +1,22 @@
+//package com.kakao.app;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@Getter
+//@Setter
+//@ToString
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class KakaoUser {
+//		
+//	private String userid;
+//	private String pwd;
+//	private String name;
+//	private String email;
+//	private String gender;
+//	private String birth;
+//}
