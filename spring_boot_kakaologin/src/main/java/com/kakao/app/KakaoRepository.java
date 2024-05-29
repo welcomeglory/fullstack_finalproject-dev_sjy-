7c6bb58b6f045d259b526ce9f7b742995501b6b8
welcomeglory <godbu2021@naver.com>
2024-05-29 21:28:36 +0900

    레포지토리 생성

---
+++ spring_boot_kakaologin/src/main/java/com/kakao/app/KakaoRepository.java
@@ -0,0 +1,9 @@
+/*
+ * package com.kakao.app;
+ * 
+ * import java.util.List;
+ * 
+ * public interface KakaoRepository { KakaoUser findById(String id);
+ * List<KakaoUser> findAll(); void save(KakaoUser user); void update(KakaoUser
+ * user); void deleteById(String id); }
+ */
