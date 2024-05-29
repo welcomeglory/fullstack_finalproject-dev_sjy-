5ecd3cbeeda5ce320578436dd7b0634a14f89ec3
welcomeglory <godbu2021@naver.com>
2024-05-29 21:29:03 +0900

    서비스 클래스 생성

---
+++ spring_boot_kakaologin/src/main/java/com/kakao/app/KakaoService.java
@@ -0,0 +1,9 @@
+/*
+ * package com.kakao.app;
+ * 
+ * import java.util.ArrayList;
+ * 
+ * public interface KakaoService { public String getToken(String code) throws
+ * Exception; public ArrayList<Object> getUserInfo(String access_token) throws
+ * Exception; }
+ */
